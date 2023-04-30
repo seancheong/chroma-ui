577ba825de229ab3c65ab7570514b95a030198af
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2023-04-30 19:36:31 +0900

    Add build-library npm script

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
+const config = {
+  input: 'src/components/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'umd',
+      name: 'chroma-ui',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'react/jsx-runtime': 'jsxRuntime',
+      },
+      sourcemap: false,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: false,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfigOverride: {
+        exclude: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.stories.tsx'],
+      },
+    }),
+    terser(),
+  ],
+  external: ['react', 'react-dom'],
+};
+
+export default config;
