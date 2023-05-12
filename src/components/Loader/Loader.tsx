17167820c1c3d8a2d7f9da3a4fecf7f9a6f1d8a7
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2023-05-12 23:44:18 +0900

    feat: Implement Loader component (#30)
    * Implement Loader component
    * Fix Button spec not able to recognize React
    * Fix Loader stories size prop to select control
    * Update Loader without color prop

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,48 @@
+import React, { PropsWithChildren } from 'react';
+import classnames from 'classnames';
+import { Size } from '../models';
+
+interface IProps extends React.HTMLAttributes<HTMLDivElement> {
+  background?: 'light' | 'dark';
+  size?: Size;
+  overlay?: boolean;
+  text?: string;
+}
+
+export const Loader: React.FC<PropsWithChildren<IProps>> = ({
+  background = 'light',
+  size = 8,
+  overlay = false,
+  text = 'Loading...',
+  className,
+}) => {
+  return (
+    <div
+      aria-label="Loading"
+      role="status"
+      className={classnames({
+        'absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-75':
+          overlay,
+      })}
+    >
+      <div
+        data-testid="spinner"
+        className={classnames(
+          'aspect-[1] rounded-full border-2 animate-[spin_0.7s_linear_infinite]',
+          { 'border-gray-300 border-r-black': background === 'light' },
+          {
+            'border-gray-500 border-r-white': background === 'dark' || overlay,
+          },
+          `w-${size}`,
+          { 'm-2': overlay },
+          className
+        )}
+      ></div>
+      {overlay && (
+        <span id="overlay-text" className="text-white font-light">
+          {text}
+        </span>
+      )}
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: [
+    {
+      pattern: /w-.*/,
+    },
+  ],
   theme: {
     extend: {},
   },
