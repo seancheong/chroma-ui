441879602c77a3a5b84429164a6bb936b19e371d
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2023-05-18 22:56:21 +0900

    Implement Icon component

---
+++ src/components/Icon/Icon.spec.tsx
@@ -0,0 +1,53 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Icon } from './Icon';
+
+describe('Icon Component', () => {
+  it('should render the icon with default props', () => {
+    // given
+    const icon = 'MagnifyingGlassIcon';
+
+    // when
+    render(<Icon icon={icon} />);
+
+    // then
+    expect(screen.getByLabelText(icon)).toHaveClass('w-8 text-gray-900');
+  });
+
+  it('should render white icon if background prop is set to dark', () => {
+    // given
+    const icon = 'MagnifyingGlassIcon';
+    const background = 'dark';
+
+    // when
+    render(<Icon icon={icon} background={background} />);
+
+    // then
+    expect(screen.getByLabelText(icon)).toHaveClass('text-white');
+  });
+
+  it('should render the icon with custom size', () => {
+    // given
+    const icon = 'MagnifyingGlassIcon';
+    const size = '6';
+
+    // when
+    render(<Icon icon={icon} size={size} />);
+
+    // then
+    expect(screen.getByLabelText(icon)).toHaveClass(`w-${size}`);
+  });
+
+  it('should still render the icon with the same class and props if type prop is set to outline', () => {
+    // given
+    const icon = 'MagnifyingGlassIcon';
+    const type = 'outline';
+    const size = '6';
+
+    // when
+    render(<Icon icon={icon} type={type} size={size} />);
+
+    // then
+    expect(screen.getByLabelText(icon)).toHaveClass(`w-${size} text-gray-900`);
+  });
+});
