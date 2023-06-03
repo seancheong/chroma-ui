03bfafb30d97bc58109e02d5fbf0633aad75f691
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2023-06-03 18:35:36 +0900

    Update storybook webpack config with cache busting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,31 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
+
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+  ],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: 'tag'
+    autodocs: 'tag',
   },
-  staticDirs: ['../public']
+  staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    config.output = {
+      ...config.output,
+      filename: '[name].[contenthash].iframe.bundle.js',
+      path: path.resolve(__dirname, '../storybook-static'),
+    };
+
+    return config;
+  },
 };
 export default config;
